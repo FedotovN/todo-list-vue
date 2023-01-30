fc80eed83dc10a96c668a47a081b4cf8f56832cf
nonsense <53238017+FedotovN@users.noreply.github.com>
2023-01-30 17:41:11 +0300

    Delete src directory

--- src/components/UI/index.js
+++
@@ -1,8 +0,0 @@
-import DialogWindow from './DialogWindow'
-import BaseButton from './BaseButton'
-import BaseInput from './BaseInput'
-import BaseSelect from './BaseSelect'
-export default{
-    DialogWindow, BaseButton, BaseInput,
-    BaseSelect
-}
